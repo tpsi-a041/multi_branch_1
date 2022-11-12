99264638215cd3a7c9987fdb37b18ec95f27b44d
Fabio Malizia <fmalizia@gmail.com>
2022-11-12 19:42:35 +0100

    Funzionalita_2

--- file1.py
+++ file1.py
@@ -4,3 +4,5 @@ print("Mi piace git")
 print ("Il secondo commit non si scorda mai")
 #Testiamo questa nuova Funzionalità
 print ("Io sono una funzione sperimentale")
+#Testiamo quest'altra funzionalità
+print("Io sono l'esperimento di un esperimento")
